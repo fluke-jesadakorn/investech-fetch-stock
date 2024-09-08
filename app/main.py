fd42625591b5526ca37f2a0d4eb11c0e2e38af3d
jesadakorn.k <jesadakorn.k@finstable.co.th>
2024-09-08 21:50:50 +0700

    change to cloud jobs

--- app/main.py
+++ app/main.py
@@ -1,6 +1,4 @@
 import logging
-import concurrent.futures
-from fastapi import FastAPI, HTTPException, Request
 from dotenv import load_dotenv
 import os
 from app.services.fetch_news_2 import (
@@ -22,110 +20,51 @@ logging.basicConfig(
 )
 logging.getLogger().disabled = False
 
-# Initialize FastAPI
-app = FastAPI()
+import logging
+from dotenv import load_dotenv
+from app.services.fetch_news_2 import fetch_symbols_from_mongo, fetch_and_save_news
+from app.services.fetch_and_save_symbols_1 import fetch_and_insert_symbols
+from app.services.data_processing_3 import fetch_process_save_news_items
+from app.services.fetch_price_4 import calculate_and_save_predicted_prices
+from app.services.fetch_gap_price_5 import fetch_and_save_symbols
+
+# Load environment variables from .env
+load_dotenv()
+
+# Set up logging
+logging.basicConfig(level=logging.INFO, format="%(asctime)s - %(message)s")
 
 
-@app.get("/")
-async def check_ready():
-    return {"message": "Ok"}
-
-
-@app.get("/1")
-async def fetch_and_save_symbols_endpoint():
+# Define a function to run the job based on JOB_ID
+def run_job(job_id):
     try:
-        fetch_and_insert_symbols()
-        return {"message": "Symbols fetched and saved successfully"}
-    except Exception as e:
-        logging.error(f"An error occurred: {e}")
-        raise HTTPException(
-            status_code=500, detail="An error occurred while processing the request"
-        )
-
-
-@app.get("/2")
-async def fetch_and_save_news_endpoint(request: Request):
-    try:
-        proxy_enabled = (
-            request.query_params.get("proxy_enabled", "false").lower() == "true"
-        )
-        proxy = request.query_params.get("proxy", None)
-        pool_maxsize = int(request.query_params.get("pool_maxsize", 20))
-
-        session = setup_session_with_proxy(
-            proxy_enabled=proxy_enabled, proxy=proxy, pool_maxsize=pool_maxsize
-        )
-        symbols = fetch_symbols_from_mongo()
-
-        if not symbols:
-            logging.error("No symbols fetched from MongoDB")
-            raise HTTPException(
-                status_code=500, detail="No symbols fetched from MongoDB"
-            )
+        if job_id == "1":
+            fetch_and_insert_symbols()
+            logging.info("Symbols fetched and saved successfully")
+        elif job_id == "2":
+            session = setup_session_with_proxy(proxy_enabled=False)
+            symbols = fetch_symbols_from_mongo()
+            for symbol in symbols:
+                fetch_and_save_news(session, symbol)
+            logging.info("News fetched and saved successfully")
+        elif job_id == "3":
+            fetch_process_save_news_items()
+            logging.info("News items fetched, processed, and saved successfully")
+        elif job_id == "4":
+            calculate_and_save_predicted_prices()
+            logging.info("Predicted prices calculated and saved successfully")
+        elif job_id == "5":
+            fetch_and_save_symbols()
+            logging.info("Last prices fetched and saved successfully")
         else:
-            max_workers = 10
-
-            with concurrent.futures.ThreadPoolExecutor(
-                max_workers=max_workers
-            ) as executor:
-                futures = [
-                    executor.submit(fetch_and_save_news, session, symbol)
-                    for symbol in symbols
-                ]
-                for future in concurrent.futures.as_completed(futures):
-                    try:
-                        future.result()
-                    except Exception as e:
-                        logging.error(f"An error occurred: {e}")
-
-            return {"message": "News fetched and saved successfully"}
-
+            logging.error("Invalid job ID")
     except Exception as e:
-        logging.error(f"An error occurred: {e}")
-        raise HTTPException(
-            status_code=500, detail="An error occurred while processing the request"
-        )
+        logging.error(f"An error occurred while running the job: {e}")
 
 
-@app.get("/3")
-async def fetch_process_save_news_items_endpoint():
-    try:
-        fetch_process_save_news_items()
-        return {"message": "News items fetched, processed, and saved successfully"}
-    except Exception as e:
-        logging.error(f"An error occurred: {e}")
-        raise HTTPException(
-            status_code=500, detail="An error occurred while processing the request"
-        )
-
-
-@app.get("/4")
-async def calculate_and_save_predicted_prices_endpoint():
-    try:
-        calculate_and_save_predicted_prices()
-        return {"message": "Predicted prices calculated and saved successfully"}
-    except Exception as e:
-        logging.error(f"An error occurred: {e}")
-        raise HTTPException(
-            status_code=500, detail="An error occurred while processing the request"
-        )
-
-
-@app.get("/5")
-async def fetch_and_save_last_prices_endpoint():
-    try:
-        fetch_and_save_symbols()
-        return {"message": "Last prices fetched and saved successfully"}
-    except Exception as e:
-        logging.error(f"An error occurred: {e}")
-        raise HTTPException(
-            status_code=500, detail="An error occurred while processing the request"
-        )
-
-
-# # Run the application
+# Main entry point
 if __name__ == "__main__":
-    import uvicorn
+    import os
 
-    port = int(os.getenv("PORT", 8000))
-    uvicorn.run(app, host="0.0.0.0", port=port, log_level="info")
+    job_id = os.getenv("JOB_ID", "1")  # Get the JOB_ID from environment variables
+    run_job(job_id)  # Run the corresponding job
